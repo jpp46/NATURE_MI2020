539afd0582e5e91a995f42cd9ac76bc71c3ca988
p-w-rs <powersj@msoe.edu>
2022-03-04 09:48:09 -0700

    restarting

---
+++ reset_dirs.jl
@@ -0,0 +1,63 @@
+using Base
+
+run(`mkdir genomes_closed_0`)
+for j in 0:60
+	DIR = "genomes_closed_0/worker_$(lpad(j, 2, "0"))"
+	run(`mkdir $DIR`)
+	open("$DIR/checkpoint.txt", "w") do f
+		write(f, "0")
+	end
+end
+
+run(`mkdir genomes_closed_15`)
+for j in 0:60
+	DIR = "genomes_closed_15/worker_$(lpad(j, 2, "0"))"
+	run(`mkdir $DIR`)
+	open("$DIR/checkpoint.txt", "w") do f
+		write(f, "0")
+	end
+end
+
+
+run(`mkdir genomes_semi_0`)
+for j in 0:60
+	DIR = "genomes_semi_0/worker_$(lpad(j, 2, "0"))"
+	run(`mkdir $DIR`)
+	open("$DIR/checkpoint.txt", "w") do f
+		write(f, "0")
+	end
+end
+
+run(`mkdir genomes_semi_15`)
+for j in 0:60
+	DIR = "genomes_semi_15/worker_$(lpad(j, 2, "0"))"
+	run(`mkdir $DIR`)
+	open("$DIR/checkpoint.txt", "w") do f
+		write(f, "0")
+	end
+end
+
+
+run(`mkdir genomes_roll_hill`)
+for j in 0:60
+	DIR = "genomes_roll_hill/worker_$(lpad(j, 2, "0"))"
+	run(`mkdir $DIR`)
+	open("$DIR/checkpoint.txt", "w") do f
+		write(f, "0")
+	end
+end
+
+
+for i in 0:15
+	run(`mkdir genomes_$i`)
+	for j in 0:60
+		DIR = "genomes_$i/worker_$(lpad(j, 2, "0"))"
+		run(`mkdir $DIR`)
+		open("$DIR/checkpoint.txt", "w") do f
+			write(f, "0")
+		end
+	end
+end
+
+
+
---
+++ run_jobs.jl
@@ -0,0 +1,9 @@
+using Base
+
+run(`sbatch g0.script`)
+run(`sbatch g15.script`)
+run(`sbatch g0_closed.script`)
+run(`sbatch g15_closed.script`)
+run(`sbatch g0_semi.script`)
+run(`sbatch g15_semi.script`)
+run(`sbatch grh.script`)
