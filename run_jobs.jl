b15f3895650a490673d58bb3f381c5d2e26a26a7
p-w-rs <powersj@msoe.edu>
2022-03-14 15:35:27 -0500

    stuff

--- run_jobs.jl
+++ run_jobs.jl
@@ -1,9 +1,9 @@
 using Base
 
-run(`sbatch --array=1-60 g0.script`)
-run(`sbatch --array=1-60 g15.script`)
-run(`sbatch --array=1-60 g0_closed.script`)
-run(`sbatch --array=1-60 g15_closed.script`)
-run(`sbatch --array=1-60 g0_semi.script`)
-run(`sbatch --array=1-60 g15_semi.script`)
-run(`sbatch --array=1-60 grh.script`)
+run(`sbatch --array=1-60%10 g0.script`)
+run(`sbatch --array=1-60%10 g15.script`)
+run(`sbatch --array=1-60%10 g0_closed.script`)
+run(`sbatch --array=1-60%10 g15_closed.script`)
+run(`sbatch --array=1-60%10 g0_semi.script`)
+run(`sbatch --array=1-60%10 g15_semi.script`)
+run(`sbatch --array=1-60%10 grh.script`)
