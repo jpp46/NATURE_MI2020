e69316718f423bc360f46271bb1999d2d1aba97c
p-w-rs <powersj@msoe.edu>
2022-05-25 15:34:19 -0500

    stuff

--- t-test.jl
+++ t-test.jl
@@ -9,23 +9,45 @@ using BSON: @load
 values_hill = []
 for i in 1:60
 	global values_hill
-	DIR = "genomes_roll_hill/"
+	DIR = "genomes_roll_hill"
 	@load "$DIR/worker_$(lpad(i, 2, "0"))_results.bson" results
 	fit = results[end][3]
 	push!(values_hill, fit)
 end
 
-values_15 = []
+values_hill = []
 for i in 1:60
 	global values_hill
-	DIR = "genomes_15/"
+	DIR = "genomes_roll_hill"
+	open("$DIR/worker_$(lpad(i, 2, "0"))_best.txt") do f
+		fit = parse(Float64, read(f, String))
+		push!(values_hill, fit)
+	end
+end
+
+values_15 = []
+for i in 1:60
+	global values_15
+	DIR = "genomes_15"
 	@load "$DIR/worker_$(lpad(i, 2, "0"))_results.bson" results
 	fit = results[end][3]
 	push!(values_15, fit)
 end
 
+values_15 = []
+for i in 1:60
+	global values_15
+	DIR = "genomes_15"
+	open("$DIR/worker_$(lpad(i, 2, "0"))_best.txt") do f
+		fit = parse(Float64, read(f, String))
+		push!(values_15, fit)
+	end
+end
+
 x = values_hill
 y = values_15
+println(x)
+println(y)
 nx, ny = length(x), length(y)
 xbar = StatsBase.mean(x)-StatsBase.mean(y)
 varx, vary = StatsBase.var(x), StatsBase.var(y)
